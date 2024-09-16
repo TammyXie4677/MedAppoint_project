835daac934152142a9d45b900856da0ec0b6199d
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-09-16 11:29:22 -0400

    New springboot App

---
+++ medappoint/src/test/java/com/example/medappoint/MedappointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medappoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedappointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
