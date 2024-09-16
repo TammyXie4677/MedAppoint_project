e2f70046fa52fa5ddaac04230aefa33f3d55a3f2
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-09-16 09:04:09 -0400

    create a java webapp

---
+++ medappoint_demo/src/test/java/com/example/medappoint_demo/MedappointDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medappoint_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedappointDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
