7a8f572db676da93c660689178ca7916d41c8ebc
FFinJA <161335409+FFinJA@users.noreply.github.com>
2024-09-19 07:41:12 -0400

    midterm submit

--- src/main/java/com/example/medappointdemo/MedappointdemoApplication.java
+++ src/main/java/com/example/medappointdemo/MedappointdemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedappointdemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MedappointdemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MedappointdemoApplication.class, args);
+	}
 
 }
--- src/main/java/com/example/medappointdemo/controller/UserController.java
+++
@@ -1,5 +0,0 @@
-package com.example.medappointdemo.controller;
-
-public class UserController {
-
-}
