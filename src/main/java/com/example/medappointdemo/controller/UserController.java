a7f3dd345de9d99521a9e43da3c6003076251bda
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-09-19 09:53:05 -0400

    Update UserController

--- src/main/java/com/example/medappointdemo/controller/UserController.java
+++ src/main/java/com/example/medappointdemo/controller/UserController.java
@@ -1,5 +1,12 @@
 package com.example.medappointdemo.controller;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class UserController {
+    @GetMapping("/Home")
+    public String home() {
+        return "index"; // This will return index.html
+    }
 }
